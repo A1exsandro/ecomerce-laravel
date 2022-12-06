c69d928298512168bb7bf8b65ada9b4724d34ae2
A1exsandro <alexsandro.acp@gmail.com>
2022-12-06 09:20:10 -0400

    begin with layout

--- resources/views/categoria.blade.php
+++ resources/views/categoria.blade.php
@@ -1,29 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+@extends("layout")
+
+@section("content")
     <h2>Categorias</h2>
 
     @if(isset($listCategorias) && count($listCategorias) > 0)
         <ul>
+            <li><a href="{{ route('categoria') }}">Todas</a></li>
             @foreach($listCategorias as $cat)
-                <li>{{ $cat->categoria }}</li>
+                <li>
+                    <a href="{{ route('categoria_by_id', ['idcategoria' => $cat->id ]) }}">{{ $cat->categoria }}</a>
+                </li>
             @endforeach
         </ul>
     @endif
 
-    @if(isset($list) && count($list) > 0)
-        <ul>
-            @foreach($list as $prod)
-                <li>{{ $prod->nome }}</li>
-            @endforeach
-        </ul>
-    @endif
-    
-</body>
-</html>
+    @include("_products", [ 'list' => $list ])
+@endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,45 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Ecomerce</title>
+@extends("layout")
 
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
-</head>
-<body>
-    <nav class="navbar navbar-light navbar-expand-md bg-light pl-100 pr-100 mb-5">
-      <a href="#" class="navbar-brand">MyShop</a>
-      <div class="collapse navbar-collapse">
-        <div class="navbar-nav">
-          <a href="{{ route('home') }}" class="nav-link">Home</a>
-          <a href="{{ route('categoria') }}" class="nav-link">Categorias</a>
-          <a href="{{ route('cadastrar') }}" class="nav-link">Cadastrar</a>
-        </div>
-      </div>
-
-      <a href="#" class="btn btn-sm"><i class="fa fa-shopping-cart"></i></a>
-    </nav>
-
-    <div class="container">
-      <div class="row">
-        @if(isset($list))
-          @foreach($list as $prod)
-            <div class="col-3 mb-3">
-              <div class="card">
-                <img src="{{ asset($prod->foto)}}" class="card-img-top" alt="">
-                <div class="card-body">
-                  <h6 class="card-title">{{ $prod->nome }} - R$ {{ $prod->valor }}</h6>
-                  <a href="#" class="btn btn-sm btn-secondary">Adicionar Item</a>
-                </div>
-              </div>
-            </div>
-          @endforeach
-        @endif
-      </div>
-    </div>
-</body>
-</html>
+@section("content")
+  @include("_products", [ 'list' => $list ])
+@endsection
+     
